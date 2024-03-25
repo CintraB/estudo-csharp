6a9df057f5c368d9f107201026d19bd430f549f0
Cristhian Cintra <64995266+CintraB@users.noreply.github.com>
2024-03-25 12:29:31 -0300

    Estruturando o projeto
    Foi adicionado menu e opções de navegação.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,66 @@
 ﻿// Screen Sound
+using System.Linq.Expressions;
+
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 
-void ExibirMensagemDeBoasVindas()
+void ExibirLogo()
 {
-Console.WriteLine("****************************");
+Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+Console.WriteLine("");
 Console.WriteLine(mensagemDeBoasVindas);
-Console.WriteLine("****************************");
+
 }
 
-ExibirMensagemDeBoasVindas();
+void ExibirOpcoesDoMenu(){
+    ExibirLogo();
+    
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite 5 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch  (opcaoEscolhidaNumerica)
+    {
+        case 1: RegistrarBandas();
+            break;
+        case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 5: Console.WriteLine("Você saiu " + opcaoEscolhidaNumerica);
+            break;
+        default: Console.WriteLine("Opção inválida");
+            break;
+    }
+}
+
+
+void RegistrarBandas()
+{
+    Console.Clear();
+    Console.WriteLine("REGISTRO DE BANDAS");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+ExibirLogo();
+ExibirOpcoesDoMenu();
