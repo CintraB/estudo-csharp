f605e812443e96293ff5d6336337209a5382ebdd
Cristhian Cintra <64995266+CintraB@users.noreply.github.com>
2024-03-25 14:10:55 -0300

    Refatorando menus e adicionando lista
    Foi criado a lista de bandas para ser populada, e os menus foram refatorados para escalarem automaticamente.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using System.Linq.Expressions;
 
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+List<string> listaDasBandas = new List<string> {"U2", "Metallica", "Iron Maiden"};
+
 
 void ExibirLogo()
 {
@@ -21,7 +23,7 @@ Console.WriteLine(mensagemDeBoasVindas);
 
 void ExibirOpcoesDoMenu(){
     ExibirLogo();
-    
+
     Console.WriteLine("\nDigite 1 para registrar uma banda");
     Console.WriteLine("Digite 2 para mostrar todas as bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
@@ -36,7 +38,7 @@ void ExibirOpcoesDoMenu(){
     {
         case 1: RegistrarBandas();
             break;
-        case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+        case 2: MostrarBandas();
             break;
         case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
             break;
@@ -53,14 +55,38 @@ void ExibirOpcoesDoMenu(){
 void RegistrarBandas()
 {
     Console.Clear();
-    Console.WriteLine("REGISTRO DE BANDAS");
+    ExibirTituloDaOpcao("REGISTRO DAS BANDAS");
     Console.Write("Digite o nome da banda que deseja registrar: ");
     string nomeDaBanda = Console.ReadLine()!;
-    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
-    Thread.Sleep(2000);
+    listaDasBandas.Add(nomeDaBanda);
+    Console.WriteLine($"\nA banda {nomeDaBanda} foi registrada com sucesso!");
+    Thread.Sleep(1200);
     Console.Clear();
     ExibirOpcoesDoMenu();
 }
 
+void MostrarBandas()
+{
+    Console.Clear();
+    ExibirTituloDaOpcao("LISTA DE BANDAS CADASTRADAS");
+    foreach (string banda in listaDasBandas)
+    {
+        Console.WriteLine("Banda: "+ banda);
+    }
+    Console.WriteLine("\nDigite qualquer tecla para voltar ao menu principal");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+void ExibirTituloDaOpcao(string titulo)
+{
+    int qtdLetras = titulo.Length;
+    string asteriscos = string.Empty.PadLeft(qtdLetras,'*');
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asteriscos + "\n");
+
+}
 ExibirLogo();
 ExibirOpcoesDoMenu();
